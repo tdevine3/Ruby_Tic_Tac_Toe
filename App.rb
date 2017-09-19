00dd73491f2eea545fe2aa9bbef6fb20e37235bd
Teddy\Toby <tdevine3@mix.wvu.edu>
2017-09-19 18:58:28 -0400

    Added partial AI code and added test for the Choose_Memory function.

--- App.rb
+++ App.rb
@@ -1,16 +1,25 @@
 def think(state)
-  memory=Memory.new
+  memories = []
+  state.each_with_index do |square, index|
+    if square != 0
+      next
+    end
+    thinking_state = state.dup
+    thinking_state[index] = turn(thinking_state)
+    memories.push(think(thinking_state))
+  end  
   
-
-  
+  return choose_memory(memories, turn(state))
 end
 
 def turn(state)
-  
   if state.count(1) > state.count(2) 
     return 2
   else
     return 1
   end
- 
+end
+
+def choose_memory(memories, player)
+    memory = Memory.new
 end
--- App_test.rb
+++ App_test.rb
@@ -5,18 +5,23 @@ require_relative "Memory.rb"
 class App_test < Minitest::Test
   
   def test_think
-      state=[1, 2, 1, 0, 2, 1, 0, 0, 0]
+      state = [1, 2, 1, 0, 2, 1, 0, 0, 0]
       assert_equal(7, think(state).square)
   end
   
   def test_turn
-    state1=[1, 2, 1, 0, 2, 1, 0, 0, 0]
-    state2=[1, 2, 1, 0, 2, 0, 0, 0, 0]
-    state3=[0, 0, 0, 0, 0, 0, 0, 0, 0]
-    
+    state1 = [1, 2, 1, 0, 2, 1, 0, 0, 0]
+    state2 = [1, 2, 1, 0, 2, 0, 0, 0, 0]
+    state3 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     assert_equal(2, turn(state1))
     assert_equal(1, turn(state2))
     assert_equal(1, turn(state3))
+  end
+  
+  def test_choose_memory
+    memories=[Memory.new(1,2), Memory.new(-1,1)]
+    assert_equal(Memory.new(-1,1), choose_memory(memories, 2))
+    a1sert_equal(Memory.new(1,2), choose_memory(memories, 1))
   end
   
 end
